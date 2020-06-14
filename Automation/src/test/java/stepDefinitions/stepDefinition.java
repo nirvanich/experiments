25ddf7a15a007c9e3710495524600b22a209421b
Olex Liubota <nirvanich@gmail.com>
2020-06-14 07:52:45 +0300

    14.06 Running tests with Cucumber TestRunner

--- Automation/src/test/java/stepDefinitions/stepDefinition.java
+++ Automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -2,7 +2,6 @@ package stepDefinitions;
 
 import org.junit.runner.RunWith;
 
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,24 +11,29 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 public class stepDefinition {
 
-    @Given("^User is on landing page$")
-    public void user_is_on_landing_page() throws Throwable {
-        throw new PendingException();
+ 
+@Given("^User is on landing page$")
+    public void user_is_on_landing_page() 
+    {
+       System.out.println("Landing Page is opened");
     }
 
     @When("^User login into application with username and password$")
-    public void user_login_into_application_with_username_and_password() throws Throwable {
-        throw new PendingException();
+    public void user_login_into_application_with_username_and_password()  
+    { 
+        System.out.println("User successfully logged in");
     }
 
     @Then("^Home page is populated$")
-    public void home_page_is_populated() throws Throwable {
-        throw new PendingException();
+    public void home_page_is_populated()  
+    {
+    	System.out.println("Home Page is opened with correct URL");
     }
 
     @And("^logout button is visible$")
-    public void logout_button_is_visible() throws Throwable {
-        throw new PendingException();
+    public void logout_button_is_visible() 
+    {
+        System.out.println("Log out button is visible");
     }
 
 }
