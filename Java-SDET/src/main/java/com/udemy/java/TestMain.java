b904043c58cf8fa44687bdc63d5847324ff2a574
Olex Liubota <nirvanich@gmail.com>
2020-07-12 21:53:52 +0300

    Bubble sort optimized

--- Java-SDET/src/main/java/com/udemy/java/TestMain.java
+++ Java-SDET/src/main/java/com/udemy/java/TestMain.java
@@ -5,25 +5,34 @@ import java.util.Arrays;
 public class TestMain {
     public static void main(String[] args) {
 
-        int[] a = {10, 9, 7, 6, 4, 5, 2, 8, 3, 1};
+        //int[] a = {10, 1, 3, 2, 4, 5, 6, 8, 9, 7};
+       int[] a = {4, 3, 2, 1, 5, 6};
         String array = Arrays.toString(a);
         System.out.println(array);
         sortArray(a);
     }
 
     private static void sortArray(int[] a) {
-        int n = a.length;
-        for (int j = 0; j < n-1; j++) {
-            for (int i = 0; i < n - 1; i++) {
-                int temp;
+        int runTimes= 0;
+
+        for (int j = 1; j < a.length; j++) {
+            boolean sorted = true;
+            for (int i = 0; i < a.length - j; i++) {
+                runTimes++;
                 if (a[i] > a[i + 1]) {
-                    temp = a[i];
-                    a[i] = a[i + 1];
-                    a[i + 1] = temp;
+                    sorted = false;
+                    int temp = a[i+1];
+                    a[i+1] = a[i];
+                    a[i] = temp;
                 }
             }
+           if (sorted){
+               break;
+           }
+
         }
         System.out.println(Arrays.toString(a));
+        System.out.println(runTimes);
     }
 
 
