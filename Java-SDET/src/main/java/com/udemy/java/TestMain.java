1ab57ccec7db4713a0fef006d2738904371f0b7b
Olex Liubota <nirvanich@gmail.com>
2020-07-14 05:32:39 +0300

    Simple Interface implementation using Lambda expression

--- Java-SDET/src/main/java/com/udemy/java/TestMain.java
+++ Java-SDET/src/main/java/com/udemy/java/TestMain.java
@@ -1,15 +1,30 @@
 package com.udemy.java;
 
+import com.udemy.java.lambda.GreetingSrvice;
+import com.udemy.java.polymorphism.Dog;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static com.udemy.java.reusableMethods.Utilities.sortArray;
 
 public class TestMain {
     public static void main(String[] args) {
+        int a = 5;
+        Dog d = new Dog();
 
-       int[] a = {7,1,2,3,4,5,6,8};
-        System.out.println(Arrays.toString(a));
-        sortArray(a);
+        //regular interface implementation
+        GreetingSrvice hiService = new GreetingSrvice() {
+            @Override
+            public void greet(String name) {
+                System.out.println("Hi..." + name);
+            }
+        };
+
+        //the same implementation but using lambda expression
+        GreetingSrvice helloService = name -> System.out.println("Hello.." + name);
+
+        helloService.greet("Mike");
     }
 
 
---
+++ Java-SDET/src/main/java/com/udemy/java/lambda/GreetingSrvice.java
@@ -0,0 +1,5 @@
+package com.udemy.java.lambda;
+
+public interface GreetingSrvice {
+    void greet(String name);
+}
