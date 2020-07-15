d59f74688448fbd5555147bc8c6c1d975588a39a
Olex Liubota <nirvanich@gmail.com>
2020-07-15 06:54:41 +0300

    Implementing simple calculator using functional interfaces (hardcoded task2 implementation)

--- Java-SDET/src/main/java/com/udemy/calculator/TestMain.java
+++ Java-SDET/src/main/java/com/udemy/calculator/TestMain.java
@@ -1,10 +1,8 @@
 package com.udemy.calculator;
 
 public class TestMain {
-    public static int calculate(MathOperation mathOperation){
-        int a = 100;
-        int b = 5;
-        return mathOperation.calculate(a, b);
+    public static int calculate(int OnScreenNumber, MathOperation mathOperation, int enteredNumber){
+        return mathOperation.calculate(OnScreenNumber, enteredNumber);
     }
 
     public static void main(String[] args) {
@@ -14,11 +12,18 @@ public class TestMain {
         MathOperation multiply = (a, b) -> a * b;
         MathOperation divide = (a, b) -> a / b;
 
+        // 5 + 2 - 3 * 7 + 2 / 3
 
-        System.out.println(calculate(add));
-        System.out.println(calculate(subtract));
-        System.out.println(calculate(multiply));
-        System.out.println(calculate(divide));
+        int result = calculate(5, add, 2);
+        result = calculate(result, subtract, 3);
+        result = calculate(result, multiply, 7);
+        result = calculate(result, add, 2);
+        result = calculate(result, divide, 3);
+
+        System.out.println(result);
+
+
+
 
 
     }
