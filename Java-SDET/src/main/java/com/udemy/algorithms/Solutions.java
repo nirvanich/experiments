46bfd06f1afe93cfa7246211a71932e1e663eea9
Olex Liubota <nirvanich@gmail.com>
2020-07-23 15:18:47 +0300

    LeetCode problems: Remove Duplicates from Sorted Array

--- Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
@@ -145,4 +145,17 @@ public class Solutions {
         }
         return result;
     }
+
+    //Given a sorted array nums, remove the duplicates in-place such that each element appear only once
+    // and return the new length.
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j< nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
 }
--- Java-SDET/src/main/java/com/udemy/algorithms/TestMain.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/TestMain.java
@@ -4,8 +4,6 @@ public class TestMain {
 
     public static void main(String[] args) {
 
-
-
     }
 
 
