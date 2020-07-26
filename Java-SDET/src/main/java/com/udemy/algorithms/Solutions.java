551794d09d5a12b64c9cb85b5cb47a4261f5556b
Olex Liubota <nirvanich@gmail.com>
2020-07-26 06:52:15 +0300

    LeetCode problems: Best Time to Buy and Sell Stock II

--- Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
@@ -158,4 +158,19 @@ public class Solutions {
         }
         return i+1;
     }
+
+    //Say you have an array prices for which the ith element is the price of a given stock on day i.
+    //Design an algorithm to find the maximum profit. You may complete as many transactions as you like
+    // (i.e., buy one and sell one share of the stock multiple times).
+    //Note: You may not engage in multiple transactions at the same time
+    // (i.e., you must sell the stock before you buy again).
+    public static int maxProfit(int[] prices) {
+        int sum = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i-1] < prices[i]){
+                sum = sum + prices[i] - prices[i-1];
+            }
+        }
+        return sum;
+    }
 }
