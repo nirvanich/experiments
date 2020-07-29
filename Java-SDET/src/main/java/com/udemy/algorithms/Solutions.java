f94b8b33a407f6d6e268a9035deb6928afeb2398
Olex Liubota <nirvanich@gmail.com>
2020-07-29 19:17:31 +0300

    LeetCode problems: Create Target Array in the Given Order

--- Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
@@ -188,4 +188,17 @@ public class Solutions {
         }
         return result;
     }
+
+    //Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+    //Initially target array is empty.
+    //From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+    //Repeat the previous step until there are no elements to read in nums and index.
+    //Return the target array.
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            result.add(index[i], nums[i]);
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
 }
--- Java-SDET/src/main/java/com/udemy/algorithms/TestMain.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/TestMain.java
@@ -1,5 +1,7 @@
 package com.udemy.algorithms;
 
+import java.util.ArrayList;
+
 public class TestMain {
 
     public static void main(String[] args) {
