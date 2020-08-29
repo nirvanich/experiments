c55d419827cd8a4795ff51d95b308a1e976764ae
Olex Liubota <nirvanich@gmail.com>
2020-08-29 16:37:40 +0300

    LeetCode problems: Split a String in Balanced Strings

--- Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/Solutions.java
@@ -246,4 +246,24 @@ public class Solutions {
         }
         return flag;
     }
+
+    //Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+    //Given a balanced string s split it in the maximum amount of balanced strings.
+    //Return the maximum amount of splitted balanced strings.
+    public static int balancedStringSplit(String s) {
+        int balancedCount = 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == 'L'){
+                count++;
+            } else if (current == 'R'){
+                count--;
+            }
+            if (count == 0){
+                balancedCount++;
+            }
+        }
+        return balancedCount;
+    }
 }
--- Java-SDET/src/main/java/com/udemy/algorithms/TestMain.java
+++ Java-SDET/src/main/java/com/udemy/algorithms/TestMain.java
@@ -1,10 +1,9 @@
 package com.udemy.algorithms;
 
-import java.util.Arrays;
-
 public class TestMain {
 
     public static void main(String[] args) {
+
 
 
     }
